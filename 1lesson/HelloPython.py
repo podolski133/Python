d1ea167577ea0418951db144ba05a1420810aaee
OlegZavodtsov <podolski133@gmail.com>
2022-07-07 00:21:16 +0300

    1les

--- 1lesson/HelloPython.py
+++ 1lesson/HelloPython.py
@@ -1,1 +1,24 @@
-print('hello world')
+# print('hello world')
+
+# типы данных и переменная
+# int, float, boolean, str, list, None
+# value = None
+# print(type(value))
+# print(type(a))
+# print(type(b))
+# value = 12334
+# print(type(value))
+# s = 'hello world'
+# a = 123
+# b = 1.23
+# print(s) # вывод строки
+# print(a, '-',b, '-', s) # способы вывода:
+# print('{1} - {2} - {0}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+
+# f = False
+# print(f)
+list = ['1', '2', '3']
+col = ['hello', 1,2,4.5,True]
+print(list)
+print(col)
