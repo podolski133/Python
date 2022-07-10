c641f4fe74a800c41d7c877ea217174530803940
OlegZavodtsov <podolski133@gmail.com>
2022-07-10 20:00:42 +0300

    end 1 lesson

--- 1lesson/HelloPython.py
+++ 1lesson/HelloPython.py
@@ -79,6 +79,143 @@
 
 # либо через отрицания not так:
 
-f = [1,2,3,4]
-is_odd = not f[0] % 2 
-print(is_odd) # будет False так как 1 не чётная
+# f = [1,2,3,4]
+# is_odd = not f[0] % 2 
+# print(is_odd) # будет False так как 1 не чётная
+
+# Управляющие конструкции
+# if, if-else
+
+# нахождения максимума из двух чисел:
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+# применение elif:
+
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('Ура, это же МАША!')
+# elif username == 'Марина':
+#     print('Я так ждала Вас, Марина!')
+# elif username == 'Ильнар':
+#     print('Ильнар - топ')
+# else:
+#     print('Привет, ', username)
+
+
+# Управляющие конструкции: while
+
+# original = 23
+# inverted = 0
+# делаем инвертируемое число:
+# while original != 0: # не равен 0
+#     # далее компануем перевернутое число
+#     inverted = inverted * 10 + (original % 10)
+#     # и целочисленное делим на 10
+#     original //= 10
+# print(inverted)
+
+# while-else
+# выполняется в том случае когда основной тело цикла перестает работать 
+
+# original = 23
+# inverted = 0
+# while original != 0: # не равен 0
+#      # далее компануем перевернутое число
+#      inverted = inverted * 10 + (original % 10)
+#      # и целочисленное делим на 10
+#      original //= 10
+# else:
+#     print('Пожалуй')
+#     print('хватит )')
+# print(inverted)
+
+
+# Управляющие конструкции: for
+
+# for i in 1,2,3,4,5:
+#    print(i**2) # демонстрация квадрата этих чисел
+# получаем 1 4 9 16 25
+
+# либо так 
+
+# list = [1,2,3,4,5]
+# for i in list:
+#     print(i**2)
+
+# range
+# r = range(10)
+# for i in r:
+#     print(i)
+# выведет от 0 до 9
+
+# либо так:
+# for i in range(10): # либо указываем диапазон (1, 5)
+#     print(i)
+# выведет от 0 до 9
+
+# Немного о строках:
+
+# text = 'съешь ещё этих мягких французских булок'
+# print(len(text)) # кол-во символов в строке = 39
+# print('ещё' in text) # проверяем какое либо слово в строке = True
+# print(text.isdigit()) # проверяем являются ли все строки числами = False
+# print(text.islower()) # или являются символами нижнего регистра
+# print(text.replace('ещё' , 'ЕЩЁ')) # или например мы что то хотим заменить
+# for c in text:
+#     print(c)
+
+
+
+# help(text.istitle)  -- данная команда подсказывает что это такое
+
+
+# Срезы
+
+# text = 'съешь ещё этих мягких французских булок'
+# print(text[0])                               # c
+# print(text[1])                               # ъ
+# print(text[len(text)])                       # IndexError        
+# print(text[len(text)-1])                     # к
+# print(text[-5])                              # б
+# print(text[:])                               # съешь ещё этих мягких французких булок
+# print(text[len(text)-2:])                    # ок
+# print(text[2:9])                             # ешь ещё
+# print(text[6:-18])                           # ещё этих мягких
+# print(text[0:len(text):6])                   # сеикакл
+# print(text[::6])                             # сеикакл
+# text = text[2:9] + text[-5] + text[:2]       # ...
+
+
+# Списки
+## list = list
+
+# numbers = [1, 2, 3, 4, 5]
+# print(numbers)
+# ran = range(1, 6)
+# numbers = list(ran)
+# print(numbers)
+# numbers[0] = 10
+# print(numbers)
+# for i in numbers:
+#     i *= 2
+#     print(i)
+# print(numbers)
+
+
+# Функции
+
+def f(x):
+    if x == 1:
+        return 'Целое'
+    elif x == 2.3:
+        return 23
+    else:
+        return
+
+arg = 2.3
+print(type(f(arg)))
