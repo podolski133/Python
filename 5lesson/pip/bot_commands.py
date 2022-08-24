c2ce790fb97a4bb61f16f0e8593908fc12716f74
OlegZavodtsov <podolski133@gmail.com>
2022-08-24 21:13:11 +0300

    telegram bot

---
+++ 5lesson/pip/bot_commands.py
@@ -0,0 +1,13 @@
+from telegram import Update 
+from telegram.ext import Updater, CommandHandler, CallbackContext 
+from bot_commands import * 
+
+updater = Updater('5697273036:AAGznY7KnTOJuU4kLSPbWf4yFbjUX2L0R40') 
+updater.dispatcher.add_handler(CommandHandler('hi', hi_command)) 
+updater.dispatcher.add_handler(CommandHandler('time', time_command)) 
+updater.dispatcher.add_handler(CommandHandler('help', help_command)) 
+updater.dispatcher.add_handler(CommandHandler('sum', sum_command)) 
+
+print('server start') 
+updater.start_polling() 
+updater.idle()
